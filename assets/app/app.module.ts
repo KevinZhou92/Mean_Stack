f00f59009fd6292b6f095d3c55ff4a48cdfbce66
KevinZh92 <monsterzpc@gmail.com>
2017-02-27 22:27:16 -0500

    Update

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
+import { MessageComponent } from "./message/message.component";
+import {MessageListComponent} from "./message/message-list.component";
+import {MessageInputComponent} from "./message/message-input.component";
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        MessageComponent,
+        MessageListComponent,
+        MessageInputComponent
     ],
-    imports: [BrowserModule],
+    imports: [BrowserModule, FormsModule],
     bootstrap: [AppComponent]
 })
 export class AppModule {
--- routes/app.js
+++ routes/app.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    res.render('node');
+    res.render('index');
+
 });
-
-
 
 
 module.exports = router;
